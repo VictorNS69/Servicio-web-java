cb0cf2d915a1aa9f21f5bfac93bbfc16a6fabe62
Victor Nieves Sanchez <vnievess@gmail.com>
2019-05-14 19:11:18 +0200

    Simple script to create the aar and run the server
    Note that your tomcat server MUST be configured.
    
    It must be in "/usr/local/tomcat7"
    
    And configured with Axis2

---
+++ run_server.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+YELLOW=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+NORMAL=$(tput sgr0)
+
+#define GREEN  "\x1B[32m"
+
+printf "%40s\n" "${YELLOW}Be sure that your tomcat folder is \"/usr/local/tomcat7\"${YELLOW}"
+# Stop the tomcat server (if it is running)
+
+printf "\n%40s\n\n" "${GREEN}Stoping actual server${NORMAL}" 
+
+sudo /usr/local/tomcat7/bin/shutdown.sh
+# Directory where build.xml is
+cd JavaWebService
+# Create the aar
+printf "\n%40s\n\n" "${GREEN}Creating UserManagementWS.aar${NORMAL}"
+
+ant
+# Move where the aar is
+cd build/lib
+# Copy the file in the tomcat-axis
+sudo cp UserManagementWS.aar /usr/local/tomcat7/webapps/axis2/WEB-INF/services/
+# Run the server
+printf "\n%40s\n\n" "${GREEN}Starting server${NORMAL}"
+sudo /usr/local/tomcat7/bin/startup.sh
+
